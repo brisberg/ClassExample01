37e642fdc42531cacf48b6037a1f992fb6d27b74
Brandon Risberg <brandon.risberg@gmail.com>
2014-10-29 16:06:48 -0700

    Initial commit
    inital commit, added the bare bones car add form

---
+++ form1.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brandon
+ * Date: 10/29/2014
+ * Time: 3:51 PM
+ */
+?>
+
+<html>
+<body>
+<form name="f1" mthod="Post" action="addcar.php">
+    <p>VIN: <input type="text" name="vin"/></p>
+    <p>Year: <input type="text" name="year"/></p>
+    <p>Mileage: <input type="text" name="mileage"/></p>
+    <p><input type="submit" name="submit"/></p>
+</form>
+</body>
+</html>
