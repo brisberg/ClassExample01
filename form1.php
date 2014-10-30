b7cdcee8eafc158f7538ac75adcb5e7d73b6bc40
Brandon Risberg <brandon.risberg@gmail.com>
2014-10-29 17:15:23 -0700

    Added a script to detect bad mileage values
    Added a <script> to check if the user input a invalid mileage value.

--- form1.php
+++ form1.php
@@ -11,17 +11,30 @@ $sql = <<<SQL
 SELECT * FROM year;
 SQL;
 
-if (!$year_results = mysqli_query($db_connection, $sql)){
-    die('There was an error running the query [' . mysqli_error($db_connection). ']');
-}
+//if (!$year_results = mysqli_query($db_connection, $sql)){
+//    die('There was an error running the query [' . mysqli_error($db_connection). ']');
+//}
 
 ?>
 
 <html>
+<head>
+    <script type="text/javascript">
+        function doValidate() {
+            // Validate the form data
+            mileage = document.f1.mileage.value;
+            if (isNaN(mileage) || mileage <= 0) {
+                // Error mileage is invalid;
+                return false;
+            }
+        }
+    </script>
+</head>
 <body>
-<form name="f1" mthod="Post" action="addcar.php">
+<form name="f1" mthod="Post" action="addcar.php" onSubmit="return doValidate()">
     <p>VIN: <input type="text" name="vin"/></p>
     <select name=""year">
+        <option value="noSelect">Select a Year</option>
     <?php
         while ($row = mysqli_fetch_array($results)) {
             echo '<option value="' . $row['year'] . '">' . $row['year']. "</option>";
